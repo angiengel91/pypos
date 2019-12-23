6db6af6f1f03ce4a1fc19857045b1c15e6d7da5d
angiengel91 <angiengel.zapata@gmail.com>
2019-12-22 21:38:53 -0600

    Creación Modelo Categorias

--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    #Fecha de Creación
+    fc = models.DateTimeField(auto_now_add=True) #Esta fecha se agrega solamente 1 vez por el tipo de inicialización 
+    #Fecha de Modificación
+    fm = models.DateTimeField(auto_now=True) #Esta fecha se va a modificar en cada acción
+    #Usuario que crea
+    uc = models.ForeignKey(User, on_delete=models.CASCADE) #Esto especifica una relación directa con los usuarios que se crean en la sección admin del sitio
+    #Usuario que modifica
+    um = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract=True #Esta sentencia hace que django omita este modelo en el momento de las migraciones
--- inv/models.py
+++ inv/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from bases.models import ClaseModelo
 
-# Create your models here.
+# NOTA:
+# El campo Id lo crea DJango de forma automática, incremental, identity, primary key
+
+#Clase Categoría
+class Categoria(ClaseModelo):
+    Descripcion =  models.CharField(
+        max_length = 100,
+        help_text = 'Descripción de la categoría', #Funciona como un tooltip o un placeholder, texto que se muestra en las cajas de tipo input
+        unique = True
+    )
+    
+    #Cuando se haga referencia al modelo, se a a mostrar uno o varios campos de la clase
+    def __str__(self):
+        return '{}'.format(self.Descripcion)
+
+    #Sobreescribir el método SAVE de la clase padre, para que las descripciones se guarden en mayúsculas
+    def save(self):
+        self.Descripcion = self.Descripcion.upper()
+        super(Categoria, self).save
+
+    #Nombre de la clase en plural
+    class Meta:
+        verbose_name_plural = 'Categorias'
